335bc3e3e728371894bc2ca8d7c695a0d819f02a
Paul Grimshaw <paul.grimshaw@krakenflex.com>
2024-05-31 14:25:32 +0200

    Rename global pg variable

--- jest.testPgDbPool.ts
+++ jest.testPgDbPool.ts
@@ -3,10 +3,10 @@ import { Pool, Client } from "pg"
 import { v4 as uuid } from "uuid"
 
 export const getPostgreSqlContainer = async (): Promise<StartedPostgreSqlContainer> => {
-    if (!global.PG_CONTAINER_INSTANCE) {
-        global.PG_CONTAINER_INSTANCE = await new PostgreSqlContainer().withDatabase("postgres").start()
+    if (!global.__PG_CONTAINER_INSTANCE) {
+        global.__PG_CONTAINER_INSTANCE = await new PostgreSqlContainer().withDatabase("postgres").start()
     }
-    return global.PG_CONTAINER_INSTANCE
+    return global.__PG_CONTAINER_INSTANCE
 }
 
 export const getTestPgDatabasePool = async (): Promise<Pool> => {
