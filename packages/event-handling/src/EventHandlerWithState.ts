e11af9652cd1febd2b7759e49904024a8efac4f4
Paul Grimshaw <paul.grimshaw@krakenflex.com>
2025-02-08 23:59:55 +0100

    Remove comments

--- packages/event-handling/src/EventHandlerWithState.ts
+++ packages/event-handling/src/EventHandlerWithState.ts
@@ -1,26 +1,16 @@
 import { DcbEvent, EventEnvelope, Tags } from "@dcb-es/event-store"
 
-// interface EventHandlerWithStateDef {
-//     state: unknown
-//     eventHandlers: DcbEvent
-// }
-
-// type EventHandlerWithStateFn<TEventEnvelope extends EventEnvelope<DcbEvent>, TState> = (
-//     eventEnvelope: TEventEnvelope,
-//     state: TState
-// ) => TState | Promise<TState>
-
-// type EventHandlersWithState<TDef extends EventHandlerWithStateDef> = {
-//     [E in TDef["eventHandlers"]as E["type"]]: EventHandlerWithStateFn<EventEnvelope<E>, TDef["state"]>
-// }
-
-export interface EventHandlerWithState<TEvents extends DcbEvent<string, Tags, unknown, unknown>, TState> {
-    tagFilter?: Partial<Tags>
+export interface EventHandlerWithState<
+    TEvents extends DcbEvent<string, Tags, unknown, unknown>,
+    TState,
+    TTags extends Tags = Tags
+> {
+    tagFilter?: Partial<TTags>
     onlyLastEvent?: boolean
     init: TState
     when: {
-        [K in TEvents["type"]]: (
-            eventEnvelope: EventEnvelope<Extract<TEvents, { type: K }>>,
+        [E in TEvents as E["type"]]: (
+            eventEnvelope: EventEnvelope<Extract<TEvents, { type: E["type"] }>>,
             state: TState
         ) => TState | Promise<TState>
     }
